b3eb32d9ddb56b89d43c57c27b5933254b4d8bb5
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-31 22:16:59 +0530

    Working on Glitch Effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     AOS.init({ duration: 1000 });
   }, [])
   return (
-
+<>
     <div className="flex flex-col items-center justify-center h-[90vh] text-center px-4">
       {/* Availability Section */}
       <div className="flex items-start gap-1 py-2  rounded-lg max-w-screen-sm">
@@ -47,20 +47,29 @@ const Hero = () => {
       </div>
     </div>
 
+    
+    </div>
+
+
+
+    {/* DIGITAL */}
+
     <div className="flex  min-h-screen ">
       <div className="w-full  p-6  shadow-lg rounded-lg text-start">
-        <h1 className="text-6xl font-bold mb-4">THE FUTURE IS DIGITAL, <br /> AND I DESIGN IT.</h1>
-        <button className="relative bg-lime-500 w-16 h-8 text-black font-bold overflow-hidden transition duration-300 clip-path-polygon group">
-  <span className="absolute inset-0 bg-white transition-all duration-1000 group-hover:translate-x-[100%]"></span>
-  <span className="relative z-10">VIEW ABOUT ABM 1</span>
-</button>
-        <h2 className="text-xl font-semibold mt-4">ABOUT</h2>
-        <p className="text-gray-600 mt-2">I blend the power of no-code innovation with development expertise to bring my creative visions to life. My approach is unconventional, but my dedication to the craft is unparalleled. I don’t just build—I engineer experiences, crafting seamless solutions that bridge technology and design.
+        <h1 className="text-6xl font-bold mt-10">THE FUTURE IS DIGITAL, <br /> AND I DESIGN IT.</h1>  {/* add glitchy effect on this */}
+        <button className="relative bg-lime-500 w-48 h-12 mt-10 text-black font-bold overflow-hidden transition duration-300 clip-path-polygon2 group">
+      <span className="absolute inset-0 bg-white transition-all duration-1000 group-hover:translate-x-[100%]"></span>
+       <span className="relative mt-16 z-10">VIEW ABOUT ABM 1*</span>
+      </button>
+        <h2 className="text-2xl font-extrabold mt-20">ABOUT ABM!</h2>
+        <p className="text-gray-300 text-2xl font-mono mt-8  w-3/4 uppercase [word-spacing:0.5rem] ">I merge no-code innovation with development expertise to engineer seamless, future-ready experiences. My unconventional approach transforms challenges into opportunities, crafting AI-driven tools, automation, and full-scale applications that bridge technology and design. With a relentless dedication to my craft, I don’t just build—I create solutions that redefine possibilities.</p>
 
-I thrive on finding unexpected solutions—turning obstacles into opportunities with the right perspective. Whether it’s automation, AI-driven tools, or full-scale applications, I code the future while designing beyond limits..</p>
+<h2 className="text-2xl font-extrabold mt-20">CHALLENGES AND APPROACH</h2>
+        <p className="text-gray-300 text-2xl font-mono mt-8  w-3/4 uppercase [word-spacing:0.5rem] ">I focused on user-centered design principles, ensuring each element served a purpose while enhancing the overall visual appeal. I employed responsive design techniques to maintain consistency across all devices and optimized performance to keep load times fast.</p>
       </div>
     </div>
-    </div>
+
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ export default {
   ],
   theme: {
     extend: {
-    
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
     
     },
   },
